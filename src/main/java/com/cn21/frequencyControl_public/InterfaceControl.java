e472dacf950d25486dd371cc0a3cfd5da901b49e
xiaofeng <1183173382@qq.com>
2016-08-24 15:55:20 +0800

    增加通过http服务器接口信息的接口代码

---
+++ src/main/java/com/cn21/frequencyControl_public/InterfaceControl.java
@@ -0,0 +1,183 @@
+/**
+ *  @Title: InterfaceConTrole.java 
+ *  @Package com.cn21.FrequencyControl.module 
+ *  @Description: TODO(用一句话描述该文件做什么) 
+ *  @author chenxiaofeng
+ *  @date 2016年8月22日 下午2:28:46 
+ *  @version V1.0 
+ */
+package com.cn21.frequencyControl_public;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.alibaba.fastjson.JSONArray;
+
+/**
+ * @author chenxiaofeng
+ * @date 2016年8月22日
+ */
+public class InterfaceControl {
+	private long interface_id;
+	private long app_id;
+	private String api_name;
+	private int frequency;
+	private int timeout;
+	private char unit;
+	private int can_loan;
+	private int quota;
+	private int deleted;
+	private List<Parameter> parameters;
+	
+	/**
+	 * @return the paremeters
+	 */
+	public List<Parameter> getParameters() {
+		return parameters;
+	}
+	/**
+	 * @param paremeters the paremeters to set
+	 */
+	public void setParameters(List<Parameter> parameters) {
+		this.parameters = parameters;
+	}
+	/**
+	 * @return the interface_id
+	 */
+	public long getInterface_id() {
+		return interface_id;
+	}
+	/**
+	 * @param interface_id the interface_id to set
+	 */
+	public void setInterface_id(long interface_id) {
+		this.interface_id = interface_id;
+	}
+	/**
+	 * @return the app_id
+	 */
+	public long getApp_id() {
+		return app_id;
+	}
+	/**
+	 * @param app_id the app_id to set
+	 */
+	public void setApp_id(long app_id) {
+		this.app_id = app_id;
+	}
+	/**
+	 * @return the api_name
+	 */
+	public String getApi_name() {
+		return api_name;
+	}
+	/**
+	 * @param api_name the api_name to set
+	 */
+	public void setApi_name(String api_name) {
+		this.api_name = api_name;
+	}
+	/**
+	 * @return the frequency
+	 */
+	public int getFrequency() {
+		return frequency;
+	}
+	/**
+	 * @param frequency the frequency to set
+	 */
+	public void setFrequency(int frequency) {
+		this.frequency = frequency;
+	}
+	/**
+	 * @return the timeout
+	 */
+	public int getTimeout() {
+		return timeout;
+	}
+	/**
+	 * @param timeout the timeout to set
+	 */
+	public void setTimeout(int timeout) {
+		this.timeout = timeout;
+	}
+	/**
+	 * @return the unit
+	 */
+	public char getUnit() {
+		return unit;
+	}
+	/**
+	 * @param unit the unit to set
+	 */
+	public void setUnit(char unit) {
+		this.unit = unit;
+	}
+	/**
+	 * @return the can_loan
+	 */
+	public int getCan_loan() {
+		return can_loan;
+	}
+	/**
+	 * @param can_loan the can_loan to set
+	 */
+	public void setCan_loan(int can_loan) {
+		this.can_loan = can_loan;
+	}
+	/**
+	 * @return the quota
+	 */
+	public int getQuota() {
+		return quota;
+	}
+	/**
+	 * @param quota the quota to set
+	 */
+	public void setQuota(int quota) {
+		this.quota = quota;
+	}
+	/**
+	 * @return the deleted
+	 */
+	public int getDeleted() {
+		return deleted;
+	}
+	/**
+	 * @param deleted the deleted to set
+	 */
+	public void setDeleted(int deleted) {
+		this.deleted = deleted;
+	}
+	/**
+	 * 把jsonArray字符串转化出pojo
+	 * @param message
+	 * @return
+	 */
+	public static List<InterfaceControl> parse(String message) {
+		List<InterfaceControl> result=new ArrayList<InterfaceControl>();
+		JSONArray parseArray = JSONArray.parseArray(message);
+		ObjectMapper objectMapper = new ObjectMapper();
+		for(int i=0;i<parseArray.size();i++){
+			try {
+				result.add(objectMapper.readValue(parseArray.getString(i), InterfaceControl.class));
+			} catch (JsonParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+}
---
+++ src/main/java/com/cn21/frequencyControl_public/util/HttpUtil.java
@@ -0,0 +1,151 @@
+/**
+ *  @Title: Test.java 
+ *  @Package com.cn21.FrequencyControl 
+ *  @Description: TODO(用一句话描述该文件做什么) 
+ *  @author chenxiaofeng
+ *  @date 2016年8月8日 下午5:21:52 
+ *  @version V1.0 
+ */
+package com.cn21.frequencyControl_public.util;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import com.cn21.frequencyControl_public.InterfaceControl;
+
+public class HttpUtil {
+
+	// 读取超时
+	private final static int SOCKET_TIMEOUT = 10000;
+	// 连接超时
+	private final static int CONNECTION_TIMEOUT = 10000;
+	// 请求超时
+	private final static int REQUEST_TIMEOUT = 10000;
+	// 每个HOST的最大连接数量
+	private final static int MAX_CONN_PRE_HOST = 20;
+	// 连接池的最大连接数
+	private final static int MAX_CONN = 60;
+	// 连接池
+	private static PoolingHttpClientConnectionManager connectionManager = null;
+	//HttpClient建造器
+	private static HttpClientBuilder httpBulder = null;
+	//请求配置
+	private static RequestConfig requestConfig = null;
+	//请求地址
+	private static String IP = "http://127.0.0.1";
+	//端口号
+	private static int PORT = 8080;
+
+	static {
+		// 设置http的状态参数
+		requestConfig = RequestConfig.custom().setSocketTimeout(SOCKET_TIMEOUT)
+				.setConnectTimeout(CONNECTION_TIMEOUT)
+				.setConnectionRequestTimeout(REQUEST_TIMEOUT).build();
+
+		HttpHost target = new HttpHost(IP, PORT);
+		connectionManager = new PoolingHttpClientConnectionManager();
+		// 将最大连接数增加
+		connectionManager.setMaxTotal(MAX_CONN);
+		// 将每个路由基础的连接增加
+		connectionManager.setDefaultMaxPerRoute(MAX_CONN_PRE_HOST);
+		// 将目标主机的最大连接数增加
+		connectionManager.setMaxPerRoute(new HttpRoute(target),
+				MAX_CONN_PRE_HOST);
+		httpBulder = HttpClients.custom();
+		httpBulder.setConnectionManager(connectionManager);
+	}
+
+	/**
+	 * 获取httpClient
+	 * @return
+	 */
+	public static CloseableHttpClient getHttpClient() {
+		CloseableHttpClient httpClient = httpBulder.build();
+		return httpClient;
+	}
+
+	/**
+	 * 构造请求方法post/get
+	 * @param map
+	 * @param url
+	 * @param method
+	 * @return
+	 */
+	public static HttpUriRequest getRequestMethod(Map<String, String> map,
+			String url, String method) {
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		for (String key : map.keySet()) {
+			NameValuePair pair = new BasicNameValuePair(key, map.get(key));
+			params.add(pair);
+		}
+		HttpUriRequest reqMethod = null;
+		if ("post".equals(method)) {
+			reqMethod = RequestBuilder
+					.post()
+					.setUri(url)
+					.addParameters(
+							params.toArray(new BasicNameValuePair[params.size()]))
+					.setConfig(requestConfig).build();
+		} else if ("get".equals(method)) {
+			reqMethod = RequestBuilder
+					.get()
+					.setUri(url)
+					.addParameters(
+							params.toArray(new BasicNameValuePair[params.size()]))
+					.setConfig(requestConfig).build();
+		}
+		return reqMethod;
+	}
+
+	/**
+	 * 从服务器中拉取数据
+	 * @param url
+	 * @param method
+	 * @return
+	 * @throws IOException
+	 */
+	public static List<InterfaceControl> getFromServer(String url, String method)
+			throws IOException {
+		Map<String, String> map = new HashMap<String, String>();
+		List<InterfaceControl> interfaces=new ArrayList<InterfaceControl>();
+		HttpClient client = getHttpClient();
+		HttpUriRequest post = getRequestMethod(map, url, method);
+		HttpResponse response = client.execute(post);
+
+		if (response.getStatusLine().getStatusCode() == 200) {
+			HttpEntity entity = response.getEntity();
+			String message = EntityUtils.toString(entity, "utf-8");
+			interfaces = InterfaceControl.parse(message);
+		}
+		return interfaces;
+	}
+
+	public static void main(String[] args) {
+		String url=IP+":"+PORT+"/interface/pull/1/2";
+		try {
+			System.out.println(getFromServer(url, "get"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
